9197df459ce2552ec07f7e35dff053ba5b0abe47
J8cob1 <J8cob1@users.noreply.github.com>
2021-01-11 19:24:34 -0800

    Messily switched to using tabs and viewpager2

--- app/src/main/java/com/example/backupapp/MainActivity.java
+++ app/src/main/java/com/example/backupapp/MainActivity.java
@@ -1,80 +1,41 @@
 package com.example.backupapp;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.*;
 
 import com.google.android.material.tabs.TabLayout;
 
-
-import java.util.*;
-
 public class MainActivity extends FragmentActivity {
 
-    @Override
-    public void onBackPressed() {
-        if (viewPager.getCurrentItem() == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed();
-        } else {
-            // Otherwise, select the previous step.
-            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
-        }
-    }
-
-    // Variables
-    private HashMap<String, Integer> tabMappings = new HashMap<String, Integer>();
     private ViewPager2 viewPager;
-    private FragmentStateAdapter pagerAdapter;
+    private PageAdapter pageAdapter;
 
-    class newFragmentStateAdapter extends FragmentStateAdapter {
-        private static final int NUM_PAGES = 3;
-
-        public newFragmentStateAdapter(FragmentActivity activity) {
-            super(activity);
-        }
-
-        @Override
-        public Fragment createFragment(int position) {
-            return new SettingsFragment();
-        }
-
-        @Override
-        public int getItemCount() {
-            return this.NUM_PAGES;
-        }
-    }
-
-    // OnCreate function
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Stuff for the main activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set up viewPager
-        this.viewPager = findViewById(R.id.content_container);
-        this.pagerAdapter = new newFragmentStateAdapter(this);
-        this.viewPager.setAdapter(this.pagerAdapter);
-
-
-        // Setup Tab Switching
-        this.initializeTabMappings();
-        MainActivity thisClass = this;
         TabLayout tab_container = findViewById(R.id.tab_container);
+        MainActivity thisActivity = this;
+        this.viewPager = findViewById(R.id.content_container);
+        this.pageAdapter = new PageAdapter(this);
+        this.viewPager.setAdapter(this.pageAdapter);
+
+        // Setup the tabs to switch whenever the view pager changes the page (and vice versa)
+        // SetupWithViewPager doesn't work with ViewPager2
+        this.viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected (int position) {
+                tab_container.getTabAt(position).select();
+            }
+        });
         tab_container.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                thisClass.viewPager.setCurrentItem(tab.getPosition());
-                Log.d("Test", String.valueOf(thisClass.viewPager.getCurrentItem()));
+                thisActivity.viewPager.setCurrentItem(tab.getPosition());
             }
 
             @Override
@@ -85,41 +46,4 @@ public class MainActivity extends FragmentActivity {
         });
 
     }
-
-
-    /* *********************** */
-    /* Tab Switching Functions */
-    /* *********************** */
-
-    // Maps tabs to appropriate positions
-    // I can't seem to initialize it directly as you would an array, so it's done with a function as suggested by a stack overflow post
-    private void initializeTabMappings() {
-        this.tabMappings.put("settings", 2);
-        this.tabMappings.put("restore", 1);
-        this.tabMappings.put("backup", 0);
-    }
-
-    // Shows a tab, one that corresponds to a given string
-    /*private void showTab(String selectedTab) {
-        Log.d("toString", selectedTab);
-        this.viewPager.setCurrentItem();
-        if (selectedTab.equals("backup")) {
-            findViewById(R.id.backup_tab_view).setVisibility(View.VISIBLE);
-        } else if (selectedTab.equals("restore")) {
-            findViewById(R.id.restore_tab_view).setVisibility(View.VISIBLE);
-        } else {
-            findViewById(R.id.settings_tab_view).setVisibility(View.VISIBLE);
-        }
-    }
-
-    // Hides a tab, one that corresponds to a given string
-    private void hideTab(String selectedTab) {
-        if (selectedTab.equals("backup")) {
-            findViewById(R.id.backup_tab_view).setVisibility(View.INVISIBLE);
-        } else if (selectedTab.equals("restore")) {
-            findViewById(R.id.restore_tab_view).setVisibility(View.INVISIBLE);
-        } else {
-            findViewById(R.id.settings_tab_view).setVisibility(View.INVISIBLE);
-        }
-    }*/
 }
---
+++ app/src/main/java/com/example/backupapp/PageAdapter.java
@@ -0,0 +1,39 @@
+package com.example.backupapp;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.google.android.material.tabs.TabLayout;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+
+/*
+* The fragment adapter for use with ViewPager2. It is responsible for loading XML fragments
+* https://developer.android.com/guide/navigation/navigation-swipe-view-2
+* https://www.w3schools.com/java/java_enums.asp
+*/
+public class PageAdapter extends FragmentStateAdapter {
+    private static final int NUM_PAGES = 3;
+    private TabLayout tab_bar;
+
+    public PageAdapter(FragmentActivity fragment) { super(fragment); }
+
+    @Override
+    @NotNull
+    public Fragment createFragment(int position) {
+        if (position == 0) { // 0 = Backup
+            return new FragmentClass("backup");
+        } else if (position == 1) { // 1 = Restore
+            return new FragmentClass("restore");
+        } else { // 2 = Settings
+            return new FragmentClass("settings");
+        }
+    }
+
+    @Override
+    public int getItemCount() { return this.NUM_PAGES; }
+}
